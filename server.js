4296f462fc418676e64b3e62bec8c7149eb74524
Saaad-tariq <saadi.cheema1@gmail.com>
2020-12-05 20:37:12 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+
+var express = require('express');
+var example = require('./example.js');
+
+
+var app = express();
+
+var PORT = 3000;
+
+app.get('/', function(req, res) {
+    example.run();
+    res.status(200).send('Hello world');
+});
+
+app.listen(PORT, function() {
+    console.log('Server is running on PORT:',PORT);
+});
