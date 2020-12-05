d4ce02e2099087b6482ce726782990461f43cbed
Saad Tariq <65291188+Saaad-tariq@users.noreply.github.com>
2020-12-06 01:15:55 +0500

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-  user          : process.env.NODE_ORACLEDB_USER || "ACCSTATUSER",
+  user          : process.env.NODE_ORACLEDB_USER || "",
   // Get the password from the environment variable
   // NODE_ORACLEDB_PASSWORD.  The password could also be a hard coded
   // string (not recommended), or it could be prompted for.
   // Alternatively use External Authentication so that no password is
   // needed.
-  password      : "OraCl3#123",
+  password      : "",
   // For information on connection strings see:
   // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
-  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "prismscan.mobilink.net.pk:1350/mapp",
+  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "",
   // Setting externalAuth is optional.  It defaults to false.  See:
   // https://oracle.github.io/node-oracledb/doc/api.html#extauth
   externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
